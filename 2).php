3f2a6b1c8eb530057d5a64326c50b4a3a48eb7dc
Pavel <35433420+Pashkaatata@users.noreply.github.com>
2020-06-08 15:27:49 +0300

    Delete 2).php

--- 2).php
+++
@@ -1,9 +0,0 @@
-<?php
-//2) Необходимо поменять местами числовые значения в строке $s.
-
-//$s = ‘This server has 386 GB RAM and 5000 GB storage’
-
-$a = 386;
-$b = 5000;
-$s = "This server has $b. GB RAM and $a. GB storage" ;
-echo $s;
